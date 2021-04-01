6a1b9a7dadcd1f7f531666c51a739716f3c6e4c1
Alexandre Setta <alexsetta@gmail.com>
2021-04-01 07:54:56 -0300

    timezone

--- waves.py
+++ waves.py
@@ -1,47 +1,51 @@
 from bs4 import BeautifulSoup
 import requests
+import datetime
 import time
-import mail 
+import pytz
+import mail
+
 
 class Wave(object):
-	def __init__(self, size, direction):
-		self.size = size
-		self.direction = direction
-    
-	def normalize(self, txt):
-		return txt.replace(" ", "").replace("\n", "")
+    def __init__(self, size, direction):
+        self.size = size
+        self.direction = direction
 
-	def show(self):
-		txt = "Tamanho: {size} - Direção: {direction}"
-		return txt.format(size=self.size, direction=self.direction)
+    def normalize(self, txt):
+        return txt.replace(" ", "").replace("\n", "")
+
+    def show(self):
+        txt = "Tamanho: {size} - Direção: {direction}"
+        return txt.format(size=self.size, direction=self.direction)
+
 
 w = Wave("", "")
 url = "https://www.waves.com.br/surf/ondas/picos/maxim/"
 teste = False
 ultimo = ""
 sleep = 1800
+tz = pytz.timezone('America/Sao_Paulo')
 
 while True:
-	dia =  time.strftime('%Y-%m-%d', time.localtime())
-	hora = time.strftime('%H', time.localtime())
-	print(dia, hora, "Verificando")
+	now = datetime.datetime.now(tz=tz)
+	dia = now.strftime('%Y-%m-%d')
+	hora = now.strftime('%H')
+	print(now.strftime('%Y-%m-%d %H:%M:%S'), "Verificando")
 	if (hora >= "06" and dia != ultimo):
-		print(dia, hora, "Enviando")
+		print(now.strftime('%Y-%m-%d %H:%M:%S'), "Enviando")
 		ultimo = dia
 		page = requests.get(url, headers={'User-Agent': 'XYZ/3.0'}).content
 		soup = BeautifulSoup(page, "html.parser")
 
-		results = soup(id="forecast_wave_size")	
+		results = soup(id="forecast_wave_size")
 		rows = results[0].findAll("span")
 		w.size = w.normalize(rows[0].get_text())
-		
-		results = soup(id="forecast_wave_direction")	
+
+		results = soup(id="forecast_wave_direction")
 		w.direction = w.normalize(results[0].get_text())
-		
+
 		mail.send(['alexsetta@gmail.com'], "Previsão para hoje", w.show())
-	
-	print("Aguardando próxima janela")
+
+	print(now.strftime('%Y-%m-%d %H:%M:%S'), "Aguardando próxima execução")
 	time.sleep(sleep)
 	continue
-
-	
